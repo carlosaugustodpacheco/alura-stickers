2f636ae3c85dff7a77fd5a3926ba765d807d0699
Carlos Augusto D. Pacheco <carlos.pacheco@aluno.ifsp.edu.br>
2022-07-20 19:59:14 -0300

    Finalizacao Aula 3 - Refatoracao

--- src/App.java
+++ src/App.java
@@ -1,43 +1,39 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
-import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         // fazer uma conexao HTTP e buscar os top 250 filmes
-        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var  request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
 
-        // extrair só os dados que interessam (titulo, poster, classificacao)
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
-        //System.out.println(listaDeFilmes.size());
-        //System.out.println(listaDeFilmes.get(0));
+        //String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        //ExtratorDeConteudo extrator = new ExtratorDeConteudoDoIMDB();
 
+
+        String url = "https://api.nasa.gov/planetary/apod?api_key=KqY8PoUsBPORNklWBwuf445BYjvVNhc3ZoXBlXkF&start_date=2022-06-12&end_date=2022-06-14";
+        ExtratorDeConteudo extrator = new ExtratorDeConteudoDaNasa();
+
+        var http =  new ClienteHttp();
+        String json = http.buscaDados(url);
+
+   
         // exibir e manipular os dados
-        for (Map<String,String> filme : listaDeFilmes) {
-            String urlImagem = filme.get("image");
-            String titulo = filme.get("title");
+       
+       List<Conteudo> conteudos = extrator.extraiConteudos(json);
 
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String nomeArquivo = "E:/CARLOS.PACHECO/Alura/Alura-ImersaoJava/alura-stickers/saida/" + titulo + ".png";
+        var geradora = new GeradoraDeFigurinhas();
+        for (int i = 0; i < 3; i++) {
+            
+        Conteudo conteudo = conteudos.get(i);
 
-            var geradora = new GeradoraDeFigurinhas();
+            InputStream inputStream = new URL(conteudo.getUrlImagem()).openStream();
+            String nomeArquivo = "E:/CARLOS.PACHECO/Alura/Alura-ImersaoJava/alura-stickers/saida/" + conteudo.getTitulo() + ".png";
+            
             geradora.cria(inputStream, nomeArquivo);
 
-            System.out.println(titulo);
+            System.out.println(conteudo.getTitulo());
             System.out.println();
         }
+        }
     }
-}
